eb69228072c281121b06957acd174f0f4b6dc979
Evgeniy.Skiba <Skiba.Evgeniy@hotmail.com>
2018-03-07 18:24:43 +0200

    fixed bundles problem

--- UserManagmentMvc/UserManagmentMvc/App_Start/BundleConfig.cs
+++ UserManagmentMvc/UserManagmentMvc/App_Start/BundleConfig.cs
@@ -11,23 +11,22 @@ namespace UserManagmentMvc.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/datetimepicker").Include(
                        "~/scripts/jquery.datetimepicker.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modalForm").Include(
-                      "~/scripts/jquery.modalForm.js"));
+                      "~/scripts/modalForm.js"));
 
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
