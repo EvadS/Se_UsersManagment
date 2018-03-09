a19ab5276a575cfaec4bb4593dee8e0bc932633a
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2018-03-09 20:48:13 +0200

    start work with unit tests

---
+++ UserManagmentMvc/UserManagment.WebTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UserManagment.DAL.Abstract;
+
+namespace UserManagment.WebTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mock<IUserRepository> repo = new Mock<IUserRepository>();
+
+          
+        }
+    }
+}
