eab82b0cfb7469ec79d6d8be30072f8cfd6301a0
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2018-03-09 20:35:26 +0200

    changed routes for pagging

--- UserManagmentMvc/UserManagmentMvc/App_Start/RouteConfig.cs
+++ UserManagmentMvc/UserManagmentMvc/App_Start/RouteConfig.cs
@@ -10,6 +10,18 @@ namespace UserManagmentMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "",
+                defaults: new { controller = "Home", action = "Index", page = 1 }
+            );
+
+            routes.MapRoute(
+                name: "pager",
+                url: "page{page}",
+                defaults: new { controller = "Home", action = "Index", page = @"\d+" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
